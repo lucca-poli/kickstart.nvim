cce97dd53e9c04961c7c049a2fa36a7974b49611
Luiz Carlos de Azevedo Santana <Luiz Carlos de Azevedo Santana>
2024-12-25 11:54:07 -0300

    Moved remaps to file

---
+++ lua/setup/init.lua
@@ -0,0 +1,2 @@
+require("setup.options") -- First to be called because it defines mapleader
+require("setup.remap") -- First to be called because it defines mapleader
---
+++ lua/setup/remap.lua
@@ -0,0 +1,22 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- vim.keymap.set("n", "<leader>f", function()
+--     vim.lsp.buf.format()
+-- end, { desc = "[F]ormat code" })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Reasonable yanking
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>p", "\"+p")
